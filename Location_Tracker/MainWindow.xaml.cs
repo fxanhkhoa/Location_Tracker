8a24641706d5542b85cf304eb90c7c9328527622
fxanhkhoa <fxanhkhoa@gmail.com>
2017-12-24 00:26:09 +0700

    .update UI + GoogleMap

--- Location_Tracker/MainWindow.xaml.cs
+++ Location_Tracker/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace Location_Tracker
 {
@@ -21,6 +22,8 @@ namespace Location_Tracker
     public partial class MainWindow : Window
     {
         String sURL = AppDomain.CurrentDomain.BaseDirectory + "html/Google_Maps_Satellite.html";
+        string filePath_satellite = AppDomain.CurrentDomain.BaseDirectory + "html/Google_Maps_Satellite.html";
+        string filePath_terran = AppDomain.CurrentDomain.BaseDirectory + "html/Google_Maps_Terran.html";
         public MainWindow()
         {
             InitializeComponent();
@@ -36,6 +39,31 @@ namespace Location_Tracker
         private void setupObjectForScripting(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void refresh_button_Click(object sender, RoutedEventArgs e)
+        {
+            //var file = new List<string>(File.ReadAllLines(sURL));
+            //string s = "abcd";
+            //file.RemoveAt(19);
+            //file.Insert(19, s);
+            //File.WriteAllLines(sURL, file.ToArray());
+
+            GetMap(11.4708344, 106.9748374);
+            webBrowser1.Refresh();
+        }
+
+        private void GetMap(double lat, double lon)
+        {
+            string command = "        var myLatlng = new google.maps.LatLng(" + lat.ToString() + "," + lon.ToString() + ")";
+            string title = "            title:\"" + lat.ToString() + "," + lon.ToString() + "\"";
+
+            var file = new List<string>(File.ReadAllLines(sURL));
+            file.RemoveAt(41); // title of Lat and Lon
+            file.Insert(41, title);
+            file.RemoveAt(19); // Function Get map from Lat and Lon
+            file.Insert(19, command);
+            File.WriteAllLines(sURL, file.ToArray());
         }
     }
 }
